8fb3dfb549f62b784212a1955dbca7b3ad876a78
Debasmit Das <debasmit.das@gmail.com>
2016-10-02 02:43:42 -0400

    SIFT and Harris Corner Detection Code Added

---
+++ hw4/harris_code.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  1 15:16:21 2016
+
+@author: debasmit
+"""
+
+
+import cv2
+import numpy as np
+import math
+
+def harris(img,sigma,level,k,th=0.3):
+	""" function for harris corner detection 
+	"""
+ 
+	# We find the size of the Haar Filter 
+        Wsize=int(math.ceil(math.ceil(4*level*sigma)/2)*2)
+        HaarX=np.concatenate((-np.ones((Wsize,Wsize/2)), np.ones((Wsize,Wsize/2))),axis=1)
+        HaarY=np.concatenate((np.ones((Wsize/2,Wsize)), -np.ones((Wsize/2,Wsize))),axis=0)
+        
+        Ix=cv2.filter2D(img,-1,HaarX)
+        Iy=cv2.filter2D(img,-1,HaarY)
+        ht=img.shape[0]
+        wd=img.shape[1]
+        newW= int(math.ceil(math.ceil(5*level*sigma)/2)*2)+1
+        newhalfW= int(math.ceil(newW/2))
+        Cresp=np.zeros((img.shape),dtype='float')
+	for i in range(newhalfW,ht-newhalfW+1):
+	  for j in range(newhalfW,wd-newhalfW+1):
+             Ix_ij=Ix[i-newhalfW:i+int(math.floor(newW/2)),j-newhalfW:j+int(math.floor(newW/2))]
+             Iy_ij=Iy[i-newhalfW:i+int(math.floor(newW/2)),j-newhalfW:j+int(math.floor(newW/2))]
+             Mat=np.zeros((2,2),dtype='float')
+             Mat[0,0]=np.sum(np.square(Ix_ij))
+             Mat[1,1]=np.sum(np.square(Iy_ij))
+             Mat[0,1]=np.sum(np.multiply(Ix_ij,Iy_ij)); Det=np.linalg.det(Mat);tr=np.trace(Mat);Cresp[i,j]=Det-k*(tr**2)
+	         
+    # Till here, the corner response of the images are found         
+	         
+	
+	#Non-maximal supression is carried out to remove multiple corners
+     	
+	Wsup=35
+	corners=[]
+	
+     	for i in range(Wsup/2,ht-Wsup/2+1):
+         for j in range(Wsup/2,wd-Wsup/2+1):
+		Crespsub=Cresp[i-Wsup/2:i+Wsup/2+1,j-Wsup/2:j+Wsup/2+1]
+		if Cresp[i,j]==np.max(Crespsub) and Cresp[i,j]> th*np.max(np.abs(Cresp)):
+			corners.append([i,j])
+	return np.asarray(corners)
+
+
+def plotimg(corners,img1,img2):
+	    #This function is used for plotting the concatenated images and the matched points
+    	img=np.zeros((max(img1.shape[0],img2.shape[0]),img1.shape[1]+img2.shape[1],3))
+    	img[:img1.shape[0], :img1.shape[1]]=img1
+    	img[:img2.shape[0], img1.shape[1]:img1.shape[1]+img2.shape[1]]=img2
+	out=img
+
+	for coord in corners:
+		cv2.circle(out,(coord[1],coord[0]),5,(255,0,0),2)
+		cv2.circle(out,(img1.shape[1]+coord[3],coord[2]),5,(255,0,0),2)
+		cv2.line(out,(coord[1],coord[0]),(img1.shape[1]+coord[3],coord[2]), (0,255,0))
+	return out
+
+
+def SSD(C1,C2,img1,img2,W,ssd,tssd):
+        """ Function for SSD to match points between feature points of 2 images and return the corresponding points
+        """
+	W_half=int(W/2);
+	if img1.shape[0]>img2.shape[0] or img1.shape[1]>img2.shape[1]:
+		
+		src=C2
+		dest=C1
+		src_img=img2
+		dest_img=img1
+		fl=1
+	else:
+		
+		src=C1
+		dest=C2
+		src_img=img1
+		dest_img=img2
+		fl=0
+  #SSD Matrix for different feature points of 2 images
+	ssdmat=np.zeros((len(src),len(dest)),dtype='float')
+	for i in range(0,len(src)):
+		for j in range(0,len(dest)):
+			src_local=src_img[src[i,0]-W_half:src[i,0]+W_half+1,src[i,1]-W_half:src[i,1]+W_half+1]
+			dest_local=dest_img[dest[j,0]-W_half:dest[j,0]+W_half+1,dest[j,1]-W_half:dest[j,1]+W_half+1]
+			ssdmat[i,j]=np.sum(np.square(np.subtract(src_local,dest_local)))
+	pts=[]
+	
+	# This is done to choose significant feature points and to remove many-to-one mappings
+	for i in range(0,len(src)):
+		for j in range(0,len(dest)):
+			if ssdmat[i,j]==np.min(ssdmat[i,:]) and ssdmat[i,j]<tssd*np.mean(ssdmat[:,:]):
+				loc_min=ssdmat[i,j]
+				ssdmat[i,j]=np.max(ssdmat[i,:])
+				if loc_min/np.min(ssdmat[i,:])<ssd:
+					
+					ssdmat[i,j]=loc_min					
+					if fl==0:
+						pts.append([src[i,0],src[i,1],dest[j,0],dest[j,1]])
+					elif fl == 1:
+						pts.append([dest[j,0],dest[j,1],src[i,0],src[i,1]])       
+        
+        return np.asarray(pts)
+
+def NCC(C1,C2,img1,img2,W,ncc,tncc):
+        """ Function for NCC to match points between feature points of 2 images and return the corresponding points
+        """
+	W_half=int(W/2);
+	if img1.shape[0]>img2.shape[0] or img1.shape[1]>img2.shape[1]:
+		
+		src=C2
+		dest=C1
+		src_img=img2
+		dest_img=img1
+		fl=1
+	else:
+		
+		src=C1
+		dest=C2
+		src_img=img1
+		dest_img=img2
+		fl=0
+   #NCC Matrix for different feature points of 2 images
+	nccmat=np.zeros((len(src),len(dest)),dtype='float')
+	for i in range(0,len(src)):
+		for j in range(0,len(dest)):
+			src_local=src_img[src[i,0]-W_half:src[i,0]+W_half+1,src[i,1]-W_half:src[i,1]+W_half+1]
+			dest_local=dest_img[dest[j,0]-W_half:dest[j,0]+W_half+1,dest[j,1]-W_half:dest[j,1]+W_half+1]
+			src_mean=np.mean(src_local)
+			dest_mean=np.mean(dest_local)
+			src_norm=np.subtract(src_local,src_mean)
+			dest_norm=np.subtract(dest_local,dest_mean)
+			num=np.sum(np.multiply(src_norm,dest_norm))
+			src_sq=np.sum(np.square(src_norm))
+			dest_sq=np.sum(np.square(dest_norm))
+			den=np.sqrt(src_sq*dest_sq)
+			nccmat[i,j]=num/den
+	pts=[]
+	
+	# This is done to choose significant feature points and to remove many-to-one mappings
+	for i in range(0,len(src)):
+		for j in range(0,len(dest)):
+			if nccmat[i,j]==np.max(nccmat[i,:]) and nccmat[i,j]>tncc*np.max(nccmat[:,:]):
+				loc_max=nccmat[i,j]
+				nccmat[i,j]=np.min(nccmat[i,:])
+				if loc_max/np.max(nccmat[i,:])>ncc:
+					
+					nccmat[i,j]=loc_max					
+					if fl==0:
+						pts.append([src[i,0],src[i,1],dest[j,0],dest[j,1]])
+					elif fl == 1:
+						pts.append([dest[j,0],dest[j,1],src[i,0],src[i,1]])
+					
+        return np.asarray(pts)
+
+
+if __name__ == "__main__":
+
+    
+    img1=cv2.imread("pair2/1.jpg")
+    img2=cv2.imread("pair2/2.jpg")
+    levels=1
+    sigma=1.2
+    W=24
+    k=0.04
+    ncc=0.8
+    tncc=0.6
+    ssd=0.7
+    tssd=6
+
+    img1gray=cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+    img2gray=cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    
+    C1 = harris(img1gray,sigma,levels,k,0.3)
+    C2 = harris(img2gray,sigma,levels,k,0.3)
+    
+    # Obtaining matches using NCC metrics
+    Cord = NCC(C1,C2,img1gray,img2gray,W,ncc,tncc)
+    cv2.imwrite('HarrisNCC.jpg',plotimg(Cord,img1,img2))
+    
+    
+    # Obtaining matches using SSD metrics
+    Cord = SSD(C1,C2,img1gray,img2gray,W,ssd,tssd)
+    cv2.imwrite('HarrisSSD.jpg',plotimg(Cord,img1,img2))
+    
--- hw4/sift_code.py
+++ hw4/sift_code.py
@@ -7,37 +7,35 @@ Created on Fri Sep 23 20:13:30 2016
 import cv2
 import numpy as np
 
-#cv2.line(img, pt1, pt2, color[, thickness[, lineType[, shift]]]) → None¶ # Command for drawing lines in images
+
 def imgSIFT(path):
     img=cv2.imread(path)
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    #default
-    #SIFT = cv2.xfeatures2d.SIFT_create(nfeatures=100,nOctaveLayers=3,contrastThreshold=0.04,edgeThreshold=10,sigma=1.6)
+
     #PeakThreshold=3
-    SIFT = cv2.xfeatures2d.SIFT_create(nfeatures=500,nOctaveLayers=1,contrastThreshold=0.12,edgeThreshold=10,sigma=1.6)
+    SIFT = cv2.xfeatures2d.SIFT_create(nfeatures=1000,nOctaveLayers=4,contrastThreshold=0.12,edgeThreshold=10,sigma=1.6)
     dummy = np.zeros((1,1))
-    kp, des = SIFT.detectAndCompute(gray,None)
+    kp, des = SIFT.detectAndCompute(gray,None) #This returns the keypoint pixels and their descriptors
     img = cv2.drawKeypoints(img, kp,dummy, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     kpix =[]
     i=0
     for k in kp:
         kpix.append([k.pt[0],k.pt[1]])
         i=i+1   
-    #print i
+    
     kpix=np.array(kpix)
     kpix=np.round(kpix)
     return img,kpix,des
+    #The image is returned with the image containing the sift features , the pixel locations and the 128-point descriptor
 
-#def SSD(img1,kpix1,img2,kpix2)    
-#def Euclidean(img1,kpix1, des1, img2, kpix2, des2):
-def Euclidean(l,euc):    
+def SSDimg(l,ssd,tssd):
+     #This is used for plotting the concatenated images
     h1, w1 = l[0][0].shape[:2]
     h2, w2 = l[1][0].shape[:2]
     img=np.zeros((max(h1,h2),w1+w2,3))
     img[:h1, :w1]=l[0][0]
-    img[:h2, w1:w1+w2]=l[1][0]
+    img[:h2, w1:w1+w2]=l[1][0]    
     
-       
     if l[0][1].shape[0]>=l[1][1].shape[0]:
         maxi=0;
         mini=1;
@@ -47,22 +45,20 @@ def Euclidean(l,euc):
         
     nmin=l[mini][1].shape[0]
     nmax=l[maxi][1].shape[0]    
-    
+    # This is used to create SSD Matrix for feature points between 2 images
     sift_mat=np.zeros((nmin,nmax))
     for i in range(0,nmin): 
         for j in range(0,nmax):
             sift_mat[i,j]=np.linalg.norm((l[mini][2][i,:]-l[maxi][2][j,:]))
-    
-    
-    
+                
+    SSD=np.square(sift_mat)
+    # This is done to choose significant feature points and to remove many-to-one mappings
     for i in range(0,nmin): 
-        min_j=0;
-        mindist=np.inf       
-        for j in range(0,nmax):
-            if sift_mat[i,j]==np.amin(sift_mat[i,:]) and sift_mat[i,j]<5*np.amin(np.amin(sift_mat)):
-                loc_min=sift_mat[i,j]
-                sift_mat[i,j]=np.amax(sift_mat[i,:])
-                if loc_min/np.amin(sift_mat[i,:])<euc:
+       for j in range(0,nmax):
+            if SSD[i,j]==np.amin(SSD[i,:]) and SSD[i,j]<tssd*np.min(SSD):
+                loc_min=SSD[i,j]
+                SSD[i,j]=np.amax(SSD[i,:])
+                if loc_min/np.amin(SSD[i,:])<ssd:
                     if mini==0:
                         pt1=l[0][1][i]
                         pt2=l[1][1][j]+np.array([l[0][0].shape[1],0])
@@ -72,11 +68,61 @@ def Euclidean(l,euc):
                         pt2=l[1][1][i]+np.array([l[0][0].shape[1],0]) 
                         cv2.line(img, (int(pt1[0]),int(pt1[1])), (int(pt2[0]),int(pt2[1])), (0,255,0))                      
                               
-    return img                         
-    
+    return img
+    #The img showing the concatenated images and SSD matched images is output        
+
+def NCCimg(l,ncc,tncc):
+     #This is used for plotting the concatenated images
+    h1, w1 = l[0][0].shape[:2]
+    h2, w2 = l[1][0].shape[:2]
+    img=np.zeros((max(h1,h2),w1+w2,3))
+    img[:h1, :w1]=l[0][0]
+    img[:h2, w1:w1+w2]=l[1][0]    
+#    
+    if l[0][1].shape[0]>=l[1][1].shape[0]:
+        maxi=0;
+        mini=1;
+    else:
+        maxi=1;
+        mini=0;
+   
+       
+    nmin=l[mini][1].shape[0]
+    nmax=l[maxi][1].shape[0]    
+    # This is used to create NCC Matrix for feature points between 2 images
+    NCC=np.zeros((nmin,nmax))
+    for i in range(0,nmin): 
+        for j in range(0,nmax):
+            f1=l[mini][2][i,:]-np.mean(l[mini][2][i,:])
+            f2=l[maxi][2][j,:]-np.mean(l[maxi][2][j,:])
+            NCC[i,j]=np.sum(np.multiply(f1,f2))/(((np.linalg.norm(f1)*np.linalg.norm(f2)))**0.5)
+                
+   
+    # This is done to choose significant feature points and to remove many-to-one mappings
+    for i in range(0,nmin): 
+       for j in range(0,nmax):
+            if NCC[i,j]==np.amax(NCC[i,:]) and NCC[i,j]>tncc*np.max(NCC):
+                loc_max=NCC[i,j]
+                NCC[i,j]=np.amin(NCC[i,:])
+                if loc_max/np.max(NCC[i,:])>ncc:
+                    if mini==0:
+                        pt1=l[0][1][i]
+                        pt2=l[1][1][j]+np.array([l[0][0].shape[1],0])
+                        cv2.line(img, (int(pt1[0]),int(pt1[1])), (int(pt2[0]),int(pt2[1])), (0,255,0))
+                    else:
+                        pt1=l[0][1][j]
+                        pt2=l[1][1][i]+np.array([l[0][0].shape[1],0]) 
+                        cv2.line(img, (int(pt1[0]),int(pt1[1])), (int(pt2[0]),int(pt2[1])), (0,255,0))                      
+                              
+    return img                     
+    #The img showing the concatenated images and NCC matched images is output 
+
+
 if __name__ == "__main__":
-    l1=imgSIFT('mypair/1.jpg')
-    l2=imgSIFT('mypair/2.jpg')
+    l1=imgSIFT('pair2/1.jpg')
+    l2=imgSIFT('pair2/2.jpg')
     l=[l1,l2]
-    img=Euclidean(l,0.7)
-    cv2.imwrite('sift_results/SIFTResult.jpg',img)
+    img1=NCCimg(l,0.7,0.9)
+    img2=SSDimg(l,0.7,5)
+    cv2.imwrite('sift_results/SIFTResultNCC.jpg',img1)
+    cv2.imwrite('sift_results/SIFTResultSSD.jpg',img2)    
